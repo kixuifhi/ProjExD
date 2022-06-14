bb91ddde0874637abf573e2244dc19ee7bc03fd2
hondam <c0a211109e@eedu.teu.ac.jp>
2022-06-14 15:45:25 +0900

    未完

---
+++ ex01/quiz.py
@@ -0,0 +1,28 @@
+from locale import ABDAY_1, ABDAY_3
+from re import A
+import random
+
+
+
+def shutudai():
+   a1 = "サザエの旦那の名前は?"
+   a2 = "カツオの妹の名前は?"
+   a3 =  "タラオはカツオから見てどんな関係?"
+   s = random.randint(0,2)
+   if s == 0:
+        print(a1)
+        
+   elif s == 1:
+        print(a2)
+
+   else:
+        print(a3)
+
+
+
+
+def kaito():
+    ans1 
+    ans2
+    ans3 
+    writeans = input("答えを入力してください。")
