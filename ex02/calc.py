df4ac22a69e96714c99f6317120e490555087e61
hondam <c0a211109e@eedu.teu.ac.jp>
2022-06-21 14:28:59 +0900

    ex02 end

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,12 +6,20 @@ if __name__=='__main__':
 root.title('電卓')
 root.geometry("300x500")
 
+r,c=0,0
+for num in range(9,-1,-1):
+   btn = tk.Button(root, 
+                   text=f'{num}',
+                   width=4,
+                   height=2,
+                   font=('Times New Roman',30)
+                   )
+   btn.grid(row=r,column=c)
+   c+=1
+   if (num-1)%3 ==0:
+     r+=1
+     c=0
 
-
-btn = tk.Button(root, text='9',width=4,height=2,
-font=('Times New Roman',30))
-#btn.grid(row=0,column=0)
-btn.pack()
 
 
 root.mainloop()
