a7cefd30f3185d8ff57f0e15c1218affe6f0c033
hondam <c0a211109e@eedu.teu.ac.jp>
2022-06-21 14:53:13 +0900

    06 end

--- ex02/calc.py
+++ ex02/calc.py
@@ -13,18 +13,19 @@ def button_click(event):
 
 
 
+
 if __name__=='__main__':
 
     root = tk.Tk()  
 root.title('電卓')
-root.geometry("300x500")
+#root.geometry("300x600")
 
 
 entry = tk.Entry(root, justify='right', width=10, font=("Times New Roman",40))
 entry.grid(row=0,column=0,columnspan=3)
 
 r,c=1,0
-for num in range(9,-1,-1):
+for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]):
    btn = tk.Button(root, 
                    text=f'{num}',
                    width=4,
@@ -35,7 +36,7 @@ for num in range(9,-1,-1):
    btn.bind("<1>",button_click)
    btn.grid(row=r,column=c)
    c+=1
-   if (num-1)%3 ==0:
+   if (i+1)%3 ==0:
      r+=1
      c=0
 
