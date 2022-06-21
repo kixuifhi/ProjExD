da7eb98db19506566579b19096570ffd46a755a2
hondam <c0a211109e@eedu.teu.ac.jp>
2022-06-21 15:25:42 +0900

    07 end

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,9 +7,15 @@ from matplotlib import widgets
 
 def button_click(event):
     btn = event.widget
-    num = btn['text']
+    num = btn['text'] #クリックされたボタンの文字
+    if num == '=':
+       eqn = entry.get()
+       res = eval(eqn)
+       entry.delete(0, tk.END)
+       entry.insert(tk.END,res)
+    else:
     #tkm.showinfo("",f'{num}のボタンがクリックされました')
-    entry.insert(tk.END,num)
+       entry.insert(tk.END,num)
 
 
 
@@ -24,8 +30,8 @@ root.title('電卓')
 entry = tk.Entry(root, justify='right', width=10, font=("Times New Roman",40))
 entry.grid(row=0,column=0,columnspan=3)
 
-r,c=1,0
-for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]):
+r,c=1,0 #r行 c列
+for i,num in enumerate([i for i in range(9,-1,-1)]+["+","="]):
    btn = tk.Button(root, 
                    text=f'{num}',
                    width=4,
@@ -35,10 +41,10 @@ for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]):
    
    btn.bind("<1>",button_click)
    btn.grid(row=r,column=c)
-   c+=1
-   if (i+1)%3 ==0:
-     r+=1
-     c=0
+   c += 1
+   if (i+1)%3 == 0:
+     r += 1
+     c = 0
 
 
 
