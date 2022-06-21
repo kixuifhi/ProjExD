2421549a4994935de77044157b147fe3cda267a8
hondam <c0a211109e@eedu.teu.ac.jp>
2022-06-21 14:42:20 +0900

    04 end

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,19 +3,26 @@ import tkinter.messagebox as tkm
 
 from matplotlib import widgets
 
-if __name__=='__main__':
-    
-    root = tk.Tk()  
-root.title('電卓')
-root.geometry("300x500")
+
 
 def button_click(event):
-    btn = event.widgets
+    btn = event.widget
     num = btn['text']
     tkm.showinfo("",f'{num}のボタンがクリックされました')
 
 
-r,c=0,0
+
+if __name__=='__main__':
+
+    root = tk.Tk()  
+root.title('電卓')
+root.geometry("300x500")
+
+
+entry = tk.Entry(root, justify='right', width=10, font=("Times New Roman",40))
+entry.grid(row=0,column=0,columnspan=3)
+
+r,c=1,0
 for num in range(9,-1,-1):
    btn = tk.Button(root, 
                    text=f'{num}',
@@ -23,11 +30,14 @@ for num in range(9,-1,-1):
                    height=2,
                    font=('Times New Roman',30)
                    )
+   
+   btn.bind("<1>",button_click)
    btn.grid(row=r,column=c)
    c+=1
    if (num-1)%3 ==0:
      r+=1
      c=0
+
 
 
 
