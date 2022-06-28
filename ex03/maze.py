d9b93791d829b1c4e5212740c6b75f17dcd9eb80
hondam <c0a211109e@eedu.teu.ac.jp>
2022-06-28 15:47:34 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,22 +1,38 @@
 import tkinter as tk
+import maze_maker as mm
 
 def key_down(event):
     global key
     key = event.keysym #key event
     #print(f"{key}キーが押されました")
 
-def key_up():
+def key_up(event):
     global key
     key = "" #空
 
+
+
 def main_proc():
-    global cx,cy
-    if key == "Up"    : cy -= 20
-    if key == "Down"  : cy += 20
-    if key == "Left"  : cx -= 20
-    if key == "Right" : cx += 20
+    global mx, my, cx, cy
+    delta = {
+        "":[0,0],
+        "Up":[0,-1],  #キー：押されたキー、値：移動幅リスト[x,y]
+        "Down":[0,1],
+        "Left":[-1,0],
+        "Right":[1,0]}
+    try:
+        if maze_bg[my+delta[key][1]][mx+delta[key][0]]==0: #移動先が床ならば
+            my, mx = my+delta[key][1], mx+delta[key][0]
+    except:
+        pass
+    #if key == "Up"    : my -= 1
+    #if key == "Down"  : my += 1
+    #if key == "Left"  : mx -= 1
+    #if key == "Right" : mx += 1
+    cx, cy = mx*100+50, my*100+50
     canvas.coords("tori", cx, cy)
     root.after(100,main_proc)
+
 
 if __name__ == "__main__":
     root = tk.Tk()    #ウインドウ
@@ -24,16 +40,20 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, width=1500, 
                      height=900,
                      bg = "black")
-    tori = tk.PhotoImage(file="fig/8.png")
-    cx, cy =  300, 400
-    canvas.create_image(cx, cy, image=tori, tag="tori")
     canvas.pack()
+    maze_bg = mm.make_maze(15, 9)   # 1:壁/0:床を表す
+    mm.show_maze(canvas, maze_bg)   # canvasにmaze_bgを描く
+    tori = tk.PhotoImage(file="fig/8.png")
+    mx, my = 1, 1
+    cx, cy =  mx*100+50, my*100+50
+    canvas.create_image(cx, cy, image=tori, tag="tori")
+    
+    
 
     key =  ""
     root.bind("<KeyPress>",key_down)
-    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
     
     main_proc()
-    
     root.mainloop()
     
