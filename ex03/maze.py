841ef85b1c9a8fbeca80cb991132e9295f3d30e1
hondam <c0a211109e@eedu.teu.ac.jp>
2022-06-28 17:07:45 +0900

    4限分

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 import maze_maker as mm
+import tkinter.messagebox as tkm
+import random 
+
+
 
 def key_down(event):
     global key
@@ -13,18 +17,24 @@ def key_up(event):
 
 
 def main_proc():
-    global mx, my, cx, cy
-    delta = {
+    global mx, my, cx, cy,a
+    a=1
+    delta = { #キー：押されたキー、値：移動幅リスト[x,y]
         "":[0,0],
-        "Up":[0,-1],  #キー：押されたキー、値：移動幅リスト[x,y]
+        "Up":[0,-1],  
         "Down":[0,1],
         "Left":[-1,0],
         "Right":[1,0]}
-    try:
-        if maze_bg[my+delta[key][1]][mx+delta[key][0]]==0: #移動先が床ならば
+    
+    if maze_bg[my+delta[key][1]][mx+delta[key][0]]==0: #移動先が床ならば
             my, mx = my+delta[key][1], mx+delta[key][0]
-    except:
+        
+    else:
         pass
+            
+            
+
+             
     #if key == "Up"    : my -= 1
     #if key == "Down"  : my += 1
     #if key == "Left"  : mx -= 1
@@ -32,7 +42,6 @@ def main_proc():
     cx, cy = mx*100+50, my*100+50
     canvas.coords("tori", cx, cy)
     root.after(100,main_proc)
-
 
 if __name__ == "__main__":
     root = tk.Tk()    #ウインドウ
@@ -43,17 +52,17 @@ if __name__ == "__main__":
     canvas.pack()
     maze_bg = mm.make_maze(15, 9)   # 1:壁/0:床を表す
     mm.show_maze(canvas, maze_bg)   # canvasにmaze_bgを描く
-    tori = tk.PhotoImage(file="fig/8.png")
+    r = random.randint(0,9) #0～9のランダムな数字
+    tori1 = tk.PhotoImage(file=f"fig/{r}.png") #画像
     mx, my = 1, 1
-    cx, cy =  mx*100+50, my*100+50
-    canvas.create_image(cx, cy, image=tori, tag="tori")
-    
-    
-
+    cx, cy =  mx*100+50, my*100+50 
+    canvas.create_image(cx, cy, image=tori1, tag="tori")
+    canvas.create_text(cx, cy, text="すたーと", anchor="center",fill="red") #startの追加
+    canvas.create_text(13*100+50, 7*100+50, text="ごーる", anchor="center",fill="red") #goalの追加
     key =  ""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
-    
+    root.after(20000, lambda: root.destroy()) #20秒でウィンドウを閉じる
     main_proc()
     root.mainloop()
     
