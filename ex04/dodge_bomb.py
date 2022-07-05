752fce3f45335406fe8cb439564a3bdba84fc23e
hondam <c0a211109e@eedu.teu.ac.jp>
2022-07-05 14:23:46 +0900

    3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -4,6 +4,8 @@ import sys
 
 def main():
     clock = pg.time.Clock()
+
+    # 練習1
     pg.display.set_caption("逃げり!こうかとん")
     screen_sfc = pg.display.set_mode((1600,900)) #Surface
     screen_rct = screen_sfc.get_rect()           #Rect
@@ -11,8 +13,16 @@ def main():
     bgimg_rct = bgimg_sfc.get_rect()             #Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
     
+    #練習3
+    kkimg_sfc = pg.image.load("fig/6.png")               #Surface
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0) #Surface
+    kkimg_rct = kkimg_sfc.get_rect()                    #Rect
+    kkimg_rct.center = 900,400
+
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+
         # 練習2
         for event in pg.event.get():
             if event.type == pg.QUIT:
